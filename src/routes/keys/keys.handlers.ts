2525b508974cb294b288294aa9c7997f9dc24b9f
JAfricanoT <josefranciscoat.correo@gmail.com>
2025-09-05 03:47:20 -0400

    feat: keys endpoint

---
+++ src/routes/keys/keys.handlers.ts
@@ -0,0 +1,99 @@
+import { eq } from "drizzle-orm";
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import * as HttpStatusPhrases from "stoker/http-status-phrases";
+
+import type { AppRouteHandler } from "@/lib/types";
+
+import postgres from "@/db/postgres/postgres";
+import { users } from "@/db/postgres/schema";
+import { ZOD_ERROR_CODES, ZOD_ERROR_MESSAGES } from "@/lib/constants";
+
+import type { CreateRoute, GetOneRoute, ListRoute, PatchRoute, RemoveRoute } from "./keys.routes";
+
+export const list: AppRouteHandler<ListRoute> = async (c) => {
+  const users = await postgres.query.users.findMany();
+  return c.json(users);
+};
+
+export const create: AppRouteHandler<CreateRoute> = async (c) => {
+  const task = c.req.valid("json");
+  const [inserted] = await postgres.insert(users).values(task).returning();
+  return c.json(inserted, HttpStatusCodes.OK);
+};
+
+export const getOne: AppRouteHandler<GetOneRoute> = async (c) => {
+  const { id } = c.req.valid("param");
+  const task = await postgres.query.users.findFirst({
+    where(fields, operators) {
+      return operators.eq(fields.id, id);
+    },
+  });
+
+  if (!task) {
+    return c.json(
+      {
+        message: HttpStatusPhrases.NOT_FOUND,
+      },
+      HttpStatusCodes.NOT_FOUND,
+    );
+  }
+
+  return c.json(task, HttpStatusCodes.OK);
+};
+
+export const patch: AppRouteHandler<PatchRoute> = async (c) => {
+  const { id } = c.req.valid("param");
+  const updates = c.req.valid("json");
+
+  if (Object.keys(updates).length === 0) {
+    return c.json(
+      {
+        success: false,
+        error: {
+          issues: [
+            {
+              code: ZOD_ERROR_CODES.INVALID_UPDATES,
+              path: [],
+              message: ZOD_ERROR_MESSAGES.NO_UPDATES,
+            },
+          ],
+          name: "ZodError",
+        },
+      },
+      HttpStatusCodes.UNPROCESSABLE_ENTITY,
+    );
+  }
+
+  const [task] = await postgres.update(users)
+    .set(updates)
+    .where(eq(users.id, id))
+    .returning();
+
+  if (!task) {
+    return c.json(
+      {
+        message: HttpStatusPhrases.NOT_FOUND,
+      },
+      HttpStatusCodes.NOT_FOUND,
+    );
+  }
+
+  return c.json(task, HttpStatusCodes.OK);
+};
+
+export const remove: AppRouteHandler<RemoveRoute> = async (c) => {
+  const { id } = c.req.valid("param");
+  const result = await postgres.delete(users)
+    .where(eq(users.id, id));
+
+  if (result.rowsAffected === 0) {
+    return c.json(
+      {
+        message: HttpStatusPhrases.NOT_FOUND,
+      },
+      HttpStatusCodes.NOT_FOUND,
+    );
+  }
+
+  return c.body(null, HttpStatusCodes.NO_CONTENT);
+};
