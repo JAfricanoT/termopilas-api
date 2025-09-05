7fedbc168a6169534120e20daf873831be6f3e4a
JAfricanoT <josefranciscoat.correo@gmail.com>
2025-09-05 03:26:53 -0400

    fix: drizzle config for multiples dbs

--- drizzle-pg.config.ts
+++ drizzle-pg.config.ts
@@ -3,12 +3,11 @@ import { defineConfig } from "drizzle-kit";
 import env from "@/env";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./src/db/migrations",
-  dialect: "turso",
+  schema: "./src/db/postgres/schema.ts",
+  out: "./src/db/postgres/migrations",
+  dialect: "postgresql",
   casing: "snake_case",
   dbCredentials: {
     url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
   },
 });
