060b023ab4d87de1973b22d6317ac98c66c15f7e
JAfricanoT <josefranciscoat.correo@gmail.com>
2025-09-06 17:24:16 -0400

    refactor: actions addresses

--- src/db/postgres/schemas/logs/schema.ts
+++ src/db/postgres/schemas/logs/schema.ts
@@ -1,15 +1,15 @@
 import { integer, pgTable, serial, timestamp } from "drizzle-orm/pg-core";
 
+import { actions } from "../actions/schema";
 import { devices } from "../devices/schema";
 import { identifiers } from "../identifiers/schema";
-import { role_actions } from "../roles/schema";
 import { temporary_identifiers } from "../temporary-identifiers/schema";
 
 export const identifier_logs = pgTable("identifier_logs", {
   id: serial().primaryKey().notNull(),
   identifier_id: integer().notNull().references(() => identifiers.id),
   device_id: integer().notNull().references(() => devices.id),
-  action_id: integer().notNull().references(() => role_actions.id),
+  action_id: integer().notNull().references(() => actions.id),
   timestamp: timestamp().defaultNow(),
 });
 
@@ -17,6 +17,6 @@ export const temporary_identifier_logs = pgTable("temporary_identifier_logs", {
   id: serial().primaryKey().notNull(),
   temporary_identifier_id: integer().notNull().references(() => temporary_identifiers.id),
   device_id: integer().notNull().references(() => devices.id),
-  action_id: integer().notNull().references(() => role_actions.id),
+  action_id: integer().notNull().references(() => actions.id),
   timestamp: timestamp().defaultNow(),
 });
