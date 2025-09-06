0092cb23b78ea20d775496afbde0bda2569d8a2a
JAfricanoT <josefranciscoat.correo@gmail.com>
2025-09-06 17:23:23 -0400

    refactor: temporary identifiers schema

--- src/db/postgres/schemas/temporary-identifiers/schema.ts
+++ src/db/postgres/schemas/temporary-identifiers/schema.ts
@@ -5,14 +5,15 @@ import { users } from "../users/schema";
 
 export const temporary_identifiers = pgTable("temporary_identifiers", {
   id: serial().primaryKey().notNull(),
-  temp_identifier_id: text().notNull().unique(),
+  temporary_identifier_id: text().notNull().unique(),
   factory_id: text().notNull().unique(),
+  created_by: integer().notNull().references(() => users.id),
   created_at: timestamp().defaultNow(),
 });
 
 export const temporary_identifier_bearers = pgTable("temporary_identifier_bearers", {
   id: serial().primaryKey().notNull(),
-  identifier_id: integer().notNull().references(() => temporary_identifiers.id),
+  temporary_identifier_id: integer().notNull().references(() => temporary_identifiers.id),
   user_id: integer().notNull().references(() => users.id),
   organization_id: integer().notNull().references(() => organizations.id),
   valid_from: timestamp().defaultNow(),
@@ -24,7 +25,6 @@ export const temporary_identifier_bearers = pgTable("temporary_identifier_bearer
 export const temporary_identifier_bearer_status = pgTable("temporary_identifier_bearer_status", {
   id: serial().primaryKey().notNull(),
   temporary_identifier_bearer_id: integer().notNull().references(() => temporary_identifier_bearers.id),
-  user_id: integer().notNull().references(() => users.id),
   is_active: boolean().notNull().default(false),
   created_by: integer().notNull().references(() => users.id),
   created_at: timestamp().defaultNow(),
