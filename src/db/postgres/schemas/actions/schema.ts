52f7c2f4b41a0c0c30e12978c678877911179ea4
JAfricanoT <josefranciscoat.correo@gmail.com>
2025-09-06 17:21:16 -0400

    feat: actions new schema

--- src/db/postgres/schemas/actions/schema.ts
+++ src/db/postgres/schemas/actions/schema.ts
@@ -1,33 +1,29 @@
 import { boolean, integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
+import { device_roles, user_roles } from "../roles/schema";
 import { users } from "../users/schema";
 
-export const user_roles = pgTable("user_roles", {
+export const actions = pgTable("actions", {
   id: serial().primaryKey().notNull(),
   name: text().notNull().unique(),
   description: text(),
+  created_by: integer().notNull().references(() => users.id),
   created_at: timestamp().defaultNow(),
 });
 
-export const user_role_status = pgTable("user_role_status", {
+export const action_status = pgTable("action_status", {
   id: serial().primaryKey().notNull(),
-  user_role_id: integer().notNull().references(() => user_roles.id),
+  action_id: integer().notNull().references(() => actions.id),
   is_active: boolean().notNull().default(false),
   created_by: integer().notNull().references(() => users.id),
   created_at: timestamp().defaultNow(),
 });
 
-export const device_roles = pgTable("device_roles", {
+export const action_roles = pgTable("action_roles", {
   id: serial().primaryKey().notNull(),
-  name: text().notNull().unique(),
-  description: text(),
-  created_by: integer().notNull().references(() => users.id),
-  created_at: timestamp().defaultNow(),
-});
-
-export const device_role_status = pgTable("device_role_status", {
-  id: serial().primaryKey().notNull(),
+  action_id: integer().notNull().references(() => actions.id),
   device_role_id: integer().notNull().references(() => device_roles.id),
+  user_role_id: integer().notNull().references(() => user_roles.id),
   is_active: boolean().notNull().default(false),
   created_by: integer().notNull().references(() => users.id),
   created_at: timestamp().defaultNow(),
