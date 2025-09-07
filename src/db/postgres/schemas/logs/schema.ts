a24b44e606c11a02abfcd232b720073bc66c139b
JAfricanoT <josefranciscoat.correo@gmail.com>
2025-09-07 19:49:16 -0400

    fix: temporary_identifier_logs saves temporary_identifier_bearer_id

--- src/db/postgres/schemas/logs/schema.ts
+++ src/db/postgres/schemas/logs/schema.ts
@@ -5,7 +5,7 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { actions } from "../actions/schema";
 import { devices } from "../devices/schema";
 import { identifiers } from "../identifiers/schema";
-import { temporary_identifiers } from "../identifiers/temporary/schema";
+import { temporary_identifier_bearers } from "../identifiers/temporary/schema";
 
 export const identifier_logs = pgTable("identifier_logs", {
   id: serial().primaryKey().notNull(),
@@ -17,7 +17,7 @@ export const identifier_logs = pgTable("identifier_logs", {
 
 export const temporary_identifier_logs = pgTable("temporary_identifier_logs", {
   id: serial().primaryKey().notNull(),
-  temporary_identifier_id: integer().notNull().references(() => temporary_identifiers.id),
+  temporary_identifier_bearer_id: integer().notNull().references(() => temporary_identifier_bearers.id),
   device_id: integer().notNull().references(() => devices.id),
   action_id: integer().notNull().references(() => actions.id),
   created_at: timestamp().defaultNow(),
